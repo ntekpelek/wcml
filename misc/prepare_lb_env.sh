6693c83af59d58c47d934cfe05b51818afccefb3
ntekpelek <piontek.a@gmail.com>
2015-05-08 18:47:46 +0200

    prepere_lb_env.sh mods

--- misc/prepare_lb_env.sh
+++ misc/prepare_lb_env.sh
@@ -1,14 +1,19 @@
 #! /bin/sh
 RDIR=/mnt/ramdisk/wcml-lb-volatile # ramdisk dir
-SDIR=/root/wcml-lb # static ( non-volatile ) dir
+LBDIR=/root/wcml/lb # static ( non-volatile ) dir
+APPDIR=/root/wcml/wcml_webapp
+W2PYDIR=/root/web2py
+rm $W2PYDIR/routes.py
 mkdir $RDIR
 cd $RDIR
 lb config --archive-areas "main contrib non-free" --bootappend-live "boot=live components persistence"
-cp $SDIR/config/apt/preferences config/apt
-cp $SDIR/config/package-lists/wcml.list.chroot config/package-lists
-cp -R $SDIR/config/includes.chroot/opt config/includes.chroot/
-cp -R $SDIR/config/includes.chroot/etc config/includes.chroot/
-cp $SDIR/config/hooks/9001-enable-wcml-services.hook.chroot config/hooks/
+cp $LBDIR/config/apt/preferences config/apt
+cp $LBDIR/config/package-lists/wcml.list.chroot config/package-lists
+cp -R $LBDIR/config/includes.chroot/opt config/includes.chroot/
+cp -R $W2PYDIR config/includes.chroot/opt/wcml
+cp -R $LBDIR/config/includes.chroot/etc config/includes.chroot/
+cp $LBDIR/config/hooks/9001-enable-wcml-services.hook.chroot config/hooks/
+cp $APPDIR config/includes.chroot/opt/wcml/web2py/applications/
 
 
 
